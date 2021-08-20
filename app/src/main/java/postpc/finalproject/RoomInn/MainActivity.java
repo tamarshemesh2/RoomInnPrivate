d473af049267908eab6f8fa3ce4f4bab1c552606
tamar.shemesh <tamar.shemesh2@mail.huji.ac.il>
2021-08-20 16:12:59 +0300

    adding login with google

--- app/src/main/java/postpc/finalproject/RoomInn/MainActivity.java
+++ app/src/main/java/postpc/finalproject/RoomInn/MainActivity.java
@@ -2,7 +2,12 @@ package postpc.finalproject.RoomInn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            Toast toast = Toast.makeText(this, personName, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
